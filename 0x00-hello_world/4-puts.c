879d327c007aea85a080fb04f06249083a717901
Patrick Chukwu <thepatrickchukwu@gmail.com>
2023-09-07 12:30:04 -0700

    Add file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h
+
+/** 
+ * programme prints "Programming is like building a multilingual puzzle"
+ */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+	}
