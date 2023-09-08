75cbb15d5edf29b6483b6a0c1f6c90def09c4404
Patrick Chukwu <thepatrickchukwu@gmail.com>
2023-09-08 14:44:15 -0700

    Edit file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		puts("%d is positive," n);
+		printf("%d is positive", n);
 	}
 	
 	if (n == 0) 
 	{
-		puts("%d is zero", n);
+		printf("%d is zero", n);
 	}
 	if (n < 0)
 	{
-		puts("%d is negative", n)
+		printf("%d is negative", n);
 	}
 	return (0);
 }
