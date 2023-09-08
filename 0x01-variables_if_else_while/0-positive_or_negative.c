1c4b75e667fc75aebf4ae70ec03cc305a3a2d023
Patrick Chukwu <thepatrickchukwu@gmail.com>
2023-09-08 14:49:28 -0700

    Edit file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
- * print a logical statement
- * using if statement
+ * main - function to check sign of number.
+ *
+ * Return: 0
  */
 
 int main(void)
@@ -14,18 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
-	
-	if (n == 0) 
+	if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
